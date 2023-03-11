a9e4ac82bfc477479ed92f64a9c36affc6dd349a
FallenCap <fallencap18@gmail.com>
2023-03-11 18:03:03 +0530

    Building Home and Pins component

--- Frontend/src/container/Home.jsx
+++ Frontend/src/container/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Sidebar, UserProfile } from '../components';
+import { client } from '../client';
+import logo from '../assets/Logo.png';
+import Pins from './Pins';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(true);
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initital">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(false)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo.png" className="w-28" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
