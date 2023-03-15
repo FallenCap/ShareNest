1c32f0ea0424a243f47801077e8f9e8017b28e6f
FallenCap <fallencap18@gmail.com>
2023-03-15 10:27:55 +0530

    Adding feed view

--- Frontend/src/components/Feed.jsx
+++ Frontend/src/components/Feed.jsx
@@ -1,17 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
 import MasonaryLayout from './MasonaryLayout';
 import Spinner from './Spinner';
 
 const Feed = () => {
   const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    //For the search query.
+    setLoading(true);
+    if (categoryId) {
+      //When we search something.
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      //When we are on the feed page.
+      // it will retreive the all data and show it on a descending order of creating date.
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
 
   if (loading) {
     // If the loading is true then it returns the below component.
     return <Spinner message="We are adding new ideas to your feed!" />;
   }
-  return <div></div>;
+  return <div>{pins && <MasonaryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
